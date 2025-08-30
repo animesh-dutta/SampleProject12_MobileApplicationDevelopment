cb62186f492685124c0efebaeee5e9e5b74b2b50
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2025-08-30 18:55:50 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {   
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int x = 5;
         int y = 10;
         System.out.println(x + y);
